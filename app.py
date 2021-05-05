2bd64443a058f98c1b7a310456d23b911ee5d88b
jforbis <forbisjohn@gmail.com>
2021-05-05 17:33:30 -0500

    save point: set up my flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("index.html")
+
+# @app.route("/resume", methods=['GET'])
+# def res():
+#     return render_template("resume.html")
+
+
+# @app.route("/projects", methods=['GET'])
+# def proj():
+#     return render_template("under_construction.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
